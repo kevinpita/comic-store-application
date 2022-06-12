fe20b4e67519d5b9bc5d7791e751d3cba632d5d2
Kevin Pita <69063372+kevinpita@users.noreply.github.com>
2022-06-13 00:15:31 +0200

    feat(controller): configuration controller

---
+++ src/main/java/io/github/kevinpita/comicstore/view/ConfigurationController.java
@@ -0,0 +1,99 @@
+/* Kevin Pita 2022 */
+package io.github.kevinpita.comicstore.view;
+
+import io.github.kevinpita.comicstore.Configuration;
+import io.github.kevinpita.comicstore.util.i18n;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class ConfigurationController {
+    @javafx.fxml.FXML private Button configSave;
+    @javafx.fxml.FXML private TextField configServer;
+    @javafx.fxml.FXML private Button configCancel;
+    @javafx.fxml.FXML private TextField configPassword;
+    @javafx.fxml.FXML private Button configCheck;
+
+    public void initialize() {
+        configServer.setText(Configuration.getApiUrl());
+        configPassword.setText(Configuration.getAuthToken());
+    }
+
+    @FXML
+    public boolean checkConfig() {
+        boolean validationResult = validate();
+        Alert alert;
+        if (validationResult) {
+            alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle(i18n.getString("validServerTitle"));
+            alert.setHeaderText(i18n.getString("validServerHeader"));
+        } else {
+            alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle(i18n.getString("invalidServerTitle"));
+            alert.setHeaderText(i18n.getString("invalidServerHeader"));
+            alert.setContentText(i18n.getString("invalidServerContent"));
+        }
+        alert.initOwner(configSave.getScene().getWindow());
+        alert.show();
+        return validationResult;
+    }
+
+    @FXML
+    public void cancelConfigEdit() {
+        configPassword.getScene().getWindow().hide();
+    }
+
+    @FXML
+    public void saveConfigEdit() {
+        if (!checkConfig()) {
+            return;
+        }
+        Configuration.setApiUrl(configServer.getText());
+        Configuration.setAuthToken(configPassword.getText());
+        Configuration.writeConfiguration();
+        configPassword.getScene().getWindow().hide();
+    }
+
+    public boolean validate() {
+        String server = configServer.getText();
+        String password = configPassword.getText();
+
+        if (server.isBlank()) {
+            return false;
+        }
+
+        if (!server.endsWith("/")) {
+            server += "/";
+        }
+
+        return checkServer(server, password);
+    }
+
+    private boolean checkServer(String server, String password) {
+        HttpClient client = HttpClient.newHttpClient();
+        try {
+            HttpRequest request =
+                    HttpRequest.newBuilder()
+                            .uri(URI.create(server + "api/v1/validation"))
+                            .header("Authorization", password)
+                            .build();
+
+            HttpResponse<String> response =
+                    client.send(request, HttpResponse.BodyHandlers.ofString());
+
+            if (isServerValid(response)) return true;
+        } catch (Exception ignored) {
+        }
+        return false;
+    }
+
+    private boolean isServerValid(HttpResponse<String> response) {
+        return response.statusCode() == 200
+                && response.body().contains("\"message\":\"validated\"");
+    }
+}
