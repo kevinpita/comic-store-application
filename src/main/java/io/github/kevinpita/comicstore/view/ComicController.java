4b4cb905484a5203985a5ef4f14a4140024d3d4d
Kevin Pita <69063372+kevinpita@users.noreply.github.com>
2022-06-12 20:41:02 +0200

    feat(controller): create comic controller

--- src/main/java/io/github/kevinpita/comicstore/view/ComicController.java
+++ src/main/java/io/github/kevinpita/comicstore/view/ComicController.java
@@ -1,4 +1,19 @@
 /* Kevin Pita 2022 */
 package io.github.kevinpita.comicstore.view;
 
-public class ComicController {}
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.Tooltip;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+
+public class ComicController {
+
+    @FXML private AnchorPane comicPane;
+    @FXML private Label comicTtle;
+    @FXML private ImageView comicImage;
+
+    public void initialize() {
+        Tooltip.install(comicPane, new Tooltip(comicTtle.getText()));
+    }
+}
