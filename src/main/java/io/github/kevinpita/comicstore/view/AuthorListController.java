2f6c39d87a93908e8bb591798f55b39a3778702b
Kevin Pita <69063372+kevinpita@users.noreply.github.com>
2022-06-14 02:09:48 +0200

    feat(table): theme table

--- src/main/java/io/github/kevinpita/comicstore/view/AuthorListController.java
+++ src/main/java/io/github/kevinpita/comicstore/view/AuthorListController.java
@@ -1,4 +1,8 @@
 /* Kevin Pita 2022 */
 package io.github.kevinpita.comicstore.view;
 
-public class AuthorListController {}
+public class AuthorListController {
+    @javafx.fxml.FXML private TableColumn authorNameTableColumn;
+    @javafx.fxml.FXML private TableColumn comicCountTableColumn;
+    @javafx.fxml.FXML private TableColumn authorLastNameTableColumn;
+}
