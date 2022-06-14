0ade11d276fd41010b45ccfe7578c74cb2f50a3d
Kevin Pita <69063372+kevinpita@users.noreply.github.com>
2022-06-14 05:13:26 +0200

    feat(report-view): create basic report view

--- src/main/java/io/github/kevinpita/comicstore/view/ReportListController.java
+++ src/main/java/io/github/kevinpita/comicstore/view/ReportListController.java
@@ -1,4 +1,14 @@
 /* Kevin Pita 2022 */
 package io.github.kevinpita.comicstore.view;
 
-public class ReportListController {}
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+
+public class ReportListController {
+    @javafx.fxml.FXML private ComboBox comboReportComicCollection;
+    @javafx.fxml.FXML private Button btnReportCollection;
+    @javafx.fxml.FXML private Button btnReportComicCollection;
+    @javafx.fxml.FXML private ComboBox comboReportComicAuthor;
+    @javafx.fxml.FXML private Button btnReportComicAuthor;
+    @javafx.fxml.FXML private Button btnReportComic;
+}
